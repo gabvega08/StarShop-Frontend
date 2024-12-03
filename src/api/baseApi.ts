f89d1eb77f2cf87af675d34970c77fc7ed3976d2
Kevin Latino <111583322+KevinLatino@users.noreply.github.com>
2024-12-02 21:11:52 -0600

    Update baseApi.ts

--- src/api/baseApi.ts
+++ src/api/baseApi.ts
@@ -3,7 +3,7 @@ import axios, { AxiosInstance } from "axios";
 export class BaseApi {
   protected axios: AxiosInstance;
 
-  constructor(baseUrl: string) {
+  constructor(baseUrl = process.env.NEXT_PUBLIC_AWS_URL) {
     this.axios = axios.create({
       baseURL: baseUrl, timeout: 10000,
       headers: {
