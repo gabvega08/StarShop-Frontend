d62010d923f4e3fc884344e08efe933055d36c5d
frankie-powers <franklin.power21@gmail.com>
2024-12-12 21:32:46 +0100

    without the implementation

--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -1,10 +1,8 @@
 "use client";
 import SettingsComponent from "@/components/settings/SettingsComponent";
-import LoadingSpinner from "@/components/ui/LoadingSpinner";
 export default function ProfilePage() {
   return (
     <>
-      <LoadingSpinner />
       <SettingsComponent />
     </>
   );
