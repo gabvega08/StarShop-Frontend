c7769c52a39a2c07f06df46c98d656f73114f45c
“Akshola00” <akinniyishola07@gmail.com>
2024-12-13 00:42:57 +0100

    chore: work started

--- src/components/register/steps/StepThreeContent.tsx
+++ src/components/register/steps/StepThreeContent.tsx
@@ -1,9 +1,13 @@
-import UploadPhotoProfile from "@/components/ui/UploadPhotoProfile"
+import UploadPhotoProfile from "@/components/ui/UploadPhotoProfile";
 
 export default function StepThreeContent(): JSX.Element {
-    return (
-        <>
-            <UploadPhotoProfile />
-        </>
-    )
+  return (
+    <>
+      <UploadPhotoProfile />
+      <div className="w-[428px] text-white h-[438px] rounded-[54px] border-[4px] border-[#3E3A77] flex flex-col items-center justify-between">
+        lorem
+        <div>india</div>
+      </div>
+    </>
+  );
 }
