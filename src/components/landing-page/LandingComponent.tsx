9623c9d4f692d8a8229339529728e12cdee5acd4
Robert <rsft6000@gmail.com>
2024-12-12 16:47:34 -0600

    [FE]: Implement Landing Page Screen and tailwind color added

--- src/components/landing-page/LandingComponent.tsx
+++ src/components/landing-page/LandingComponent.tsx
@@ -1,9 +1,55 @@
 import React from 'react';
+import StarShopLanding from "../../../public/starshop-logos/StarShop-Logo-Landing.svg"
+import Link from 'next/link'
+import Image from "next/image"
 
 const LandingPageComponent: React.FC = () => {
   return (
     <>
-      <h1 className='text-white text-2xl font-bold mt-8'>Landing page</h1>
+      <div className='text-white h-[120px]'>
+        Add header here
+      </div>
+
+      <div className='p-0 flex flex-col-reverse md:flex-row w-full xl:my-[35px] xl:pl-[100px] px-[10px] md:px-[50px] pb-[100px] text-white max-w-[1600px] m-auto'>
+        <div className='w-full md:w-1/2 xl:max-w-[700px]'>
+          <h1 className='uppercase text-white text-[50px] md:text-[64px] font-extrabold leading-[50px] md:leading-[64px]'>
+            Marketplace built on stellar blockchain
+          </h1>
+          <p className='text-white text-lg sm:text-sm mt-[20px] md:mt-[35px]'>
+            StarShop enables small businesses to sell products transparently while fostering trust. Customers receive unique NFTs as digital collectibles with each purchase, and businesses earn milestone NFTs to showcase their success.
+          </p>
+          <Link
+            href="/register"
+            className='mt-[30px] md:mt-[50px] font-bold inline-block bg-white text-primary-purple px-[10px] md:px-[40px] py-[15px] rounded-[25px] hover:bg-purple-100 leading-[22.72px] text-left cursor-pointer'
+          >
+            Register Now
+          </Link>
+          <div className='flex flex-col md:flex-row mt-[50px] !text-center'>
+            <div className='mb-6'>
+              <div className="text-4xl sm:text-4xl font-semibold md:mb-2">200+</div>
+              <div className="text-lg sm:text-sm">Sellers</div>
+            </div>
+            <div className='h-16 min-w-[2px] bg-white mx-[50px] hidden md:block'></div>
+            <div className='mb-6'>
+              <div className="text-4xl sm:text-4xl font-semibold md:mb-2">2000+</div>
+              <div className="text-lg sm:text-sm">High-Quality<br className="hidden sm:block" /> products</div>
+            </div>
+            <div className='h-16 min-w-[2px] bg-white mx-[50px] hidden md:block'></div>
+            <div>
+              <div className="text-4xl sm:text-4xl font-semibold md:mb-2">30,000+</div>
+              <div className="text-lg sm:text-sm">Happy Customers</div>
+            </div>
+          </div>
+        </div>
+        <div className='mx-auto xl:m-0 w-[400px] md:w-1/2 flex justify-center items-center px-4 my-[0px] '>
+          <Image
+            src={StarShopLanding}
+            alt="StarShop Logo"
+            width={700}
+            height={20}
+          />
+        </div>
+      </div>
     </>
   );
 };
