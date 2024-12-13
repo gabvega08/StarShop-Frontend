5dedaa5135c8934c30aaa9f92f1cb99a1541ceab
Joel Vargas <vermudo.com@gmail.com>
2024-12-13 15:45:51 -0600

    feat: Add Input component for reusable form input fields with optional icon

---
+++ src/components/ui/Input.tsx
@@ -0,0 +1,42 @@
+import { FC } from "react";
+import { LucideIcon } from "lucide-react";
+
+interface InputProps {
+  label: string;
+  id: string;
+  type: string;
+  placeholder?: string;
+  name: string;
+  icon?: LucideIcon;
+}
+
+const Input: FC<InputProps> = ({
+  label,
+  id,
+  type,
+  placeholder,
+  name,
+  icon: Icon,
+}) => {
+  return (
+    <div className="flex flex-col items-center text-center space-y-2">
+      <label className="uppercase font-bold text-white text-lg" htmlFor={id}>
+        {label}
+      </label>
+      <div className="relative w-full">
+        {Icon && (
+          <Icon className="absolute left-3 top-1/2 transform -translate-y-1/2 text-white" />
+        )}
+        <input
+          className={`w-full bg-custom-light-card-border text-gray-200 text-sm p-3 pl-14 rounded-lg border-b-4 border-primary-purple`}
+          id={id}
+          type={type}
+          placeholder={placeholder}
+          name={name}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Input;
