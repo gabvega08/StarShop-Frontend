c7d07d2edaecf5481557e6db6fb8152ef7bf7ea8
MPSxDev <mparedes.digital@gmail.com>
2024-12-13 12:46:21 -0300

    feat: Add navbar items into NavBarComponent

--- src/components/ui/NavBarComponent.tsx
+++ src/components/ui/NavBarComponent.tsx
@@ -31,6 +31,35 @@ const NavbarComponent: FC = () => {
                     <img src="/starshop-logos/StarShop-Logo.svg" alt="Logo" width={120} height={120} />
                 </Link>
             </div>
+            <div className={`
+                fixed md:relative
+                top-0 md:top-auto 
+                left-0 h-full w-64
+                md:w-auto md:h-auto
+                bg-black md:bg-transparent
+                transform transition-transform duration-300 ease-in-out
+                ${isMenuOpen ? 'translate-x-0' : '-translate-x-full'}
+                md:translate-x-0
+                flex flex-col md:flex-row
+                pt-20 md:pt-0
+                px-6 md:px-6
+                gap-4 md:gap-8
+                shadow-lg md:shadow-none
+                z-40
+                md:mr-auto
+            `}>
+                {navItems.map((item) => (
+                    <Link
+                        key={item.href}
+                        href={item.href}
+                        className={`text-white hover:text-gray-300 relative pb-1 ${
+                            pathname === item.href ? 'after:absolute after:bottom-0 after:left-0 after:w-full after:h-0.5 after:bg-[#9354FF]' : ''
+                        }`}
+                    >
+                        {item.name}
+                    </Link>
+                ))}
+            </div>
         </nav>
     );
 };
