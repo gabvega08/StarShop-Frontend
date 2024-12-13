f00ed8a10df431271cc19ccee03494d5b24262db
drakkomaximo <drakkomaximo1234@gmail.com>
2024-12-12 19:20:36 -0500

    chart component has been enabled

--- src/components/dashboard/components/SellsPerMoth.tsx
+++ src/components/dashboard/components/SellsPerMoth.tsx
@@ -1,9 +1,118 @@
 import React from "react";
+import ReactApexChart from "react-apexcharts";
+import { ApexOptions } from "apexcharts";
 
-export const SellsPerMoth = () => {
+interface ChartState {
+  series: Array<{
+    name: string;
+    data: number[];
+  }>;
+  options: ApexOptions;
+}
+
+export const SellsPerMoth: React.FC = () => {
+  const [state] = React.useState<ChartState>({
+    series: [
+      {
+        name: "Page Views",
+        data: [35, 41, 62, 42, 13, 18, 29, 37, 36, 51, 32, 35],
+      },
+    ],
+    options: {
+      chart: {
+        height: 100,
+        width: 300,
+        type: "line",
+        zoom: {
+          enabled: false,
+        },
+        toolbar: {
+          show: false, // Oculta el menú de opciones
+        },
+      },
+      colors: ["#3E3A77"], // Cambia el color de la línea principal a rojo
+      dataLabels: {
+        enabled: false,
+      },
+      stroke: {
+        width: [5],
+        curve: "straight",
+      },
+      legend: {
+        show: false, // Oculta las leyendas
+      },
+      markers: {
+        size: 0,
+        hover: {
+          sizeOffset: 6,
+        },
+      },
+      xaxis: {
+        categories: [
+          "01 Jan",
+          "02 Jan",
+          "03 Jan",
+          "04 Jan",
+          "05 Jan",
+          "06 Jan",
+          "07 Jan",
+          "08 Jan",
+          "09 Jan",
+          "10 Jan",
+          "11 Jan",
+          "12 Jan",
+        ],
+        labels: {
+          show: false, // Oculta las etiquetas del eje X
+        },
+      },
+      yaxis: {
+        labels: {
+          show: false, // Oculta las etiquetas del eje Y
+        },
+      },
+      tooltip: {
+        y: [
+          {
+            title: {
+              formatter: function (val) {
+                return val + " (mins)";
+              },
+            },
+          },
+          {
+            title: {
+              formatter: function (val) {
+                return val + " per session";
+              },
+            },
+          },
+          {
+            title: {
+              formatter: function (val) {
+                return val;
+              },
+            },
+          },
+        ],
+      },
+      grid: {
+        borderColor: "#f1f1f1",
+      },
+    },
+  });
+
   return (
-    <div className="h-32 bg-[#2a2a3e] rounded-md flex items-center justify-center">
-      <p className="text-gray-400">Chart Placeholder</p>
+    <div>
+      <div id="chart">
+        <ReactApexChart
+          options={state.options}
+          series={state.series}
+          type="line"
+          height={150}
+        />
+      </div>
+      <div id="html-dist"></div>
     </div>
   );
 };
