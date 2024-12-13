c24ff48d24af2dbdb127e3fd8165adfbe9ba99a3
“Akshola00” <akinniyishola07@gmail.com>
2024-12-13 14:27:17 +0100

    feat: fix box width

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,9 @@
 import LandingPageComponent from "@/components/landing-page/LandingComponent";
-import StepThreeContent from "@/components/register/steps/StepThreeContent";
 
 export default function LadingPage() {
   return (
     <>
       <LandingPageComponent />
-      <StepThreeContent />
     </>
   );
 }
