e1dcddd3b975049c5fb34c48777a35edadf71f78
Joel Vargas <vermudo.com@gmail.com>
2024-12-13 14:46:51 -0600

    feat: Wrap SettingsComponent content in Bounded for improved layout

--- src/components/settings/SettingsComponent.tsx
+++ src/components/settings/SettingsComponent.tsx
@@ -1,9 +1,13 @@
-import React from 'react';
+import React from "react";
+import NavbarComponent from "../ui/NavBarComponent";
+import Bounded from "../ui/Bounded";
 
 const SettingsComponent: React.FC = () => {
   return (
     <>
-      <h1 className='text-white text-2xl'>profile</h1>
+      <Bounded>
+        <h1 className="text-white text-2xl">dsa</h1>
+      </Bounded>
     </>
   );
 };
