52d06b8f628bde12899ec166324b4051243c4ee9
Joel Vargas <vermudo.com@gmail.com>
2024-12-13 14:46:43 -0600

    feat: Add NavbarComponent to ProfilePage and provide layout suggestion

--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -1,8 +1,11 @@
 import SettingsComponent from "@/components/settings/SettingsComponent";
+import NavbarComponent from "@/components/ui/NavBarComponent";
 
 export default function ProfilePage() {
   return (
     <>
+      <NavbarComponent />
+      {/* Dear maintainer, here you can put this component in the layout, so you wouldn't have to paste in each component */}
       <SettingsComponent />
     </>
   );
