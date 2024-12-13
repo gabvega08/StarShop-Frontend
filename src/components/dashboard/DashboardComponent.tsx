71948b49e8bc85e59fbac882f17505729c138db7
Joel Vargas <vermudo.com@gmail.com>
2024-12-13 14:46:48 -0600

    feat: Wrap DashboardComponent content in Bounded for improved layout

--- src/components/dashboard/DashboardComponent.tsx
+++ src/components/dashboard/DashboardComponent.tsx
@@ -7,66 +7,65 @@ import { AboutStore } from "./components/AboutStore";
 import { TopProducts } from "./components/TopProducts";
 import { Statistics } from "./components/Statistics";
 import { SellsPerMonth } from "./components/SellsPerMonth";
+import Bounded from "../ui/Bounded";
 
 const DashboardComponent = () => {
   return (
-    <div className="min-h-screen space-y-24 text-white p-4 sm:p-6 lg:p-8">
-      <div className="max-w-screen-xl mx-auto px-4">
-        <section className="grid grid-cols-1 gap-12 md:grid-cols-2 xl:grid-cols-[1fr_2fr] xl:gap-28">
-          <CardContainer>
-            <div className={`flex gap-2 items-center mb-2 pl-4`}>
-              <BagIcon />
-              <h3 className="text-[20px] sm:text-[24px] lg:text-[30px] font-bold text-center text-background">
-                About your store
-              </h3>
-            </div>
+    <Bounded>
+      <section className="grid grid-cols-1 gap-12 md:grid-cols-2 xl:grid-cols-[1fr_2fr] xl:gap-28">
+        <CardContainer>
+          <div className={`flex gap-2 items-center mb-2 pl-4`}>
+            <BagIcon />
+            <h3 className="text-[20px] sm:text-[24px] lg:text-[30px] font-bold text-center text-background">
+              About your store
+            </h3>
+          </div>
 
-            <div className="flex flex-col items-center px-4 pb-4 border-4 border-custom-light-card-border rounded-[20px] sm:rounded-[24px] lg:rounded-[28px] max-h-[254px] h-screen">
-              <h4 className="text-center text-lg sm:text-xl lg:text-2xl font-semibold mt-2">
-                STRAB
-              </h4>
-              <AboutStore />
-            </div>
-          </CardContainer>
+          <div className="flex flex-col items-center px-4 pb-4 border-4 border-custom-light-card-border rounded-[20px] sm:rounded-[24px] lg:rounded-[28px] max-h-[254px] h-screen">
+            <h4 className="text-center text-lg sm:text-xl lg:text-2xl font-semibold mt-2">
+              STRAB
+            </h4>
+            <AboutStore />
+          </div>
+        </CardContainer>
 
-          <CardContainer>
-            <div className="flex flex-col items-center px-4 pb-4 border-4 border-custom-light-card-border rounded-[20px] sm:rounded-[24px] lg:rounded-[28px] max-h-[254px] h-screen">
-              <h4 className="text-center text-lg sm:text-xl lg:text-2xl font-semibold mt-2">
-                TOP PRODUCTS
-              </h4>
-              <TopProducts />
-            </div>
-          </CardContainer>
-        </section>
+        <CardContainer>
+          <div className="flex flex-col items-center px-4 pb-4 border-4 border-custom-light-card-border rounded-[20px] sm:rounded-[24px] lg:rounded-[28px] max-h-[254px] h-screen">
+            <h4 className="text-center text-lg sm:text-xl lg:text-2xl font-semibold mt-2">
+              TOP PRODUCTS
+            </h4>
+            <TopProducts />
+          </div>
+        </CardContainer>
+      </section>
 
-        <section className="grid grid-cols-1 gap-12 md:grid-cols-2 xl:grid-cols-[1fr_2fr] xl:gap-28 mt-8">
-          <CardContainer>
-            <div className={`flex gap-2 items-center mb-2 pl-4`}>
-              <FlameIcon color="#9354FF" width={41} height={33} />
-              <h3 className="text-[20px] sm:text-[24px] lg:text-[30px] font-bold text-center text-background">
-                Your store statistics
-              </h3>
-            </div>
+      <section className="grid grid-cols-1 gap-12 md:grid-cols-2 xl:grid-cols-[1fr_2fr] xl:gap-28 mt-8">
+        <CardContainer>
+          <div className={`flex gap-2 items-center mb-2 pl-4`}>
+            <FlameIcon color="#9354FF" width={41} height={33} />
+            <h3 className="text-[20px] sm:text-[24px] lg:text-[30px] font-bold text-center text-background">
+              Your store statistics
+            </h3>
+          </div>
 
-            <div className="flex flex-col items-center px-4 pb-4 border-4 border-custom-light-card-border rounded-[20px] sm:rounded-[24px] lg:rounded-[28px] max-h-[254px] h-screen">
-              <h4 className="text-center text-lg sm:text-xl lg:text-2xl font-semibold mt-2 uppercase">
-                Store statistics
-              </h4>
-              <Statistics />
-            </div>
-          </CardContainer>
+          <div className="flex flex-col items-center px-4 pb-4 border-4 border-custom-light-card-border rounded-[20px] sm:rounded-[24px] lg:rounded-[28px] max-h-[254px] h-screen">
+            <h4 className="text-center text-lg sm:text-xl lg:text-2xl font-semibold mt-2 uppercase">
+              Store statistics
+            </h4>
+            <Statistics />
+          </div>
+        </CardContainer>
 
-          <CardContainer>
-            <div className="flex flex-col items-center px-4 pb-4 border-4 border-custom-light-card-border rounded-[20px] sm:rounded-[24px] lg:rounded-[28px] max-h-[254px] h-screen">
-              <h4 className="text-center text-lg sm:text-xl lg:text-2xl font-semibold mt-2">
-                SELLS PER MONTH
-              </h4>
-              <SellsPerMonth />
-            </div>
-          </CardContainer>
-        </section>
-      </div>
-    </div>
+        <CardContainer>
+          <div className="flex flex-col items-center px-4 pb-4 border-4 border-custom-light-card-border rounded-[20px] sm:rounded-[24px] lg:rounded-[28px] max-h-[254px] h-screen">
+            <h4 className="text-center text-lg sm:text-xl lg:text-2xl font-semibold mt-2">
+              SELLS PER MONTH
+            </h4>
+            <SellsPerMonth />
+          </div>
+        </CardContainer>
+      </section>
+    </Bounded>
   );
 };
 
