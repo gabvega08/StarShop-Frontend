782950b7af1529aa43fb180e56b3e439b6b7f263
“Michael <dahformulah@gmail.com>
2025-02-26 03:51:44 +0100

    deleted billing page

--- src/app/billing/page.tsx
+++
@@ -1,9 +0,0 @@
-import TransactionHistory from "@/components/billing/transaction-history/xlm-transactionHistory";
-
-export default function TransactionsPage() {
-    return (
-        <>
-            <TransactionHistory />
-        </>
-    );
-}
