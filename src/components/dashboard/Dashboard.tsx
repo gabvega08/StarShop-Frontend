7e351115525a0e603665c7245a08034cfe0d542c
Sebastian Salazar <sebastian.salazar.solano@gmail.com>
2025-02-26 14:40:35 -0600

    feat: add the store overview component

---
+++ src/components/dashboard/Dashboard.tsx
@@ -0,0 +1,9 @@
+import StoreOverview from "@/components/dashboard/store-overview/StoreOverview";
+
+export default function DashboardPage() {
+  return (
+    <div>
+      <StoreOverview />
+    </div>
+  );
+}
