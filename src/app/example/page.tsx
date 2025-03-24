938727c538131268e321f712555793010f2308f0
Josué Brenes <123853174+JosueBrenes@users.noreply.github.com>
2025-03-23 22:33:17 -0600

    feat: delete example page

--- src/app/example/page.tsx
+++
@@ -1,9 +0,0 @@
-import TicketDetails from "@/components/features/user/support/components/TicketDetails";
-
-export default function LadingPage() {
-  return (
-    <>
-      <TicketDetails />
-    </>
-  );
-}
