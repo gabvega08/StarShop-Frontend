539791cd6f2eb20a835af681ce0e807dc9e65448
pvsaint <vpeter817@gmail.com>
2025-03-29 20:39:58 +0100

    fix: typography

--- src/components/features/user/invoice/RecentActivites.tsx
+++ src/components/features/user/invoice/RecentActivites.tsx
@@ -23,34 +23,34 @@ export default function RecentActivity({
     switch (type) {
       case "INVOICE_PAID":
         return (
-          <div className="h-8 w-8 rounded-full flex items-center justify-center">
+          <div className="h-10 w-10 rounded-full flex items-center justify-center">
             <Image
               alt="Checkmark"
               src="/icons/Overlay.png"
-              width={24}
-              height={24}
+              width={30}
+              height={30}
             />
           </div>
         );
       case "INVOICE_CREATED":
         return (
-          <div className="h-8 w-8 rounded-full flex items-center justify-center">
+          <div className="h-10 w-10 rounded-full flex items-center justify-center">
             <Image
               alt="Checkmark"
               src="/icons/Overlay (1).png"
-              width={24}
-              height={24}
+              width={30}
+              height={30}
             />
           </div>
         );
       case "PAYMENT_REMINDER_SENT":
         return (
-          <div className="h-8 w-8 rounded-full flex items-center justify-center">
+          <div className="h-10 w-10 rounded-full flex items-center justify-center">
             <Image
               alt="Checkmark"
               src="/icons/Overlay (2).png"
-              width={24}
-              height={24}
+              width={30}
+              height={30}
             />
           </div>
         );
@@ -82,8 +82,12 @@ export default function RecentActivity({
   return (
     <div className="w-full rounded-lg p-6 bg-[#de73ff09] border border-gray-800  shadow-sm shadow-[#FFFFFF1A]">
       <div className="mb-2">
-        <h2 className="text-xl font-semibold text-white">Recent Activity</h2>
-        <p className="text-gray-400 text-sm">Latest invoice actions</p>
+        <h2 className="text-[17.16px] font-semibold text-white">
+          Recent Activity
+        </h2>
+        <p className="text-[#FFFFFF99] text-[13.56px]">
+          Latest invoice actions
+        </p>
       </div>
 
       <div className="space-y-6 mt-6">
@@ -92,18 +96,20 @@ export default function RecentActivity({
             <div key={event.id} className="flex items-start gap-4">
               <div className="flex-shrink-0">{getEventIcon(event.type)}</div>
               <div className="flex-1 min-w-0">
-                <h3 className="text-white font-medium">
+                <h3 className="text-white text-[15.38px]">
                   {getEventTitle(event.type)}
                 </h3>
-                <p className="text-gray-400">{getEventDescription(event)}</p>
+                <p className="text-[#FFFFFF99] text-[13.67px]">
+                  {getEventDescription(event)}
+                </p>
               </div>
-              <div className="flex-shrink-0 text-xs text-gray-400">
+              <div className="flex-shrink-0 text-[11.81px] text-[#FFFFFF66]">
                 {formatTimeAgo(event.timestamp)}
               </div>
             </div>
           ))
         ) : (
-          <p className="text-sm text-gray-400 py-4 text-center">
+          <p className="text-[13.67px] text-[#FFFFFF99] py-4 text-center">
             No recent activity
           </p>
         )}
@@ -113,7 +119,7 @@ export default function RecentActivity({
         <div className="mt-8 text-center">
           <Link
             href="/activity"
-            className="inline-flex items-center text-sm font-medium text-gray-400 hover:text-white"
+            className="inline-flex items-center text-[13.67px] text-[#FFFFFF99] hover:text-white"
           >
             View All Activity
             <ChevronRight className="ml-1 h-4 w-4" />
