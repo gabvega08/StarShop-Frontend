9f089bf8af36d6a63d621fdcee9651cb7cc5ae33
SimpleX-T <ntmark2004@gmail.com>
2025-04-26 15:59:42 +0100

    fixed debounce function

---
+++ src/hooks/useDebounce.ts
@@ -0,0 +1,27 @@
+import { useState, useEffect } from "react";
+
+/**
+ * Custom hook to debounce a value by a specified delay
+ * @param value - The value to debounce
+ * @param delay - Delay in milliseconds (default is 500ms)
+ * @returns The debounced value
+ *
+ * This is how you can use this hook:
+ * const [searchTerm, setSearchTerm] = useState('');
+ * const debouncedSearchTerm = useDebounce(searchTerm, 300);
+ */
+export function useDebounce<T>(value: T, delay: number = 500): T {
+  const [debouncedValue, setDebouncedValue] = useState<T>(value);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setDebouncedValue(value);
+    }, delay);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [value, delay]);
+
+  return debouncedValue;
+}
