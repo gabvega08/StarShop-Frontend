fd17ad1cf7f5bc10bcd9f05379414e18e4ab113b
KevinLatino <latinokevin9@gmail.com>
2025-06-25 17:05:57 -0600

    feat: back button have been implemented

--- src/shared/components/layout/NotFound.tsx
+++ src/shared/components/layout/NotFound.tsx
@@ -1,9 +1,12 @@
+"use client"
+
 import { ArrowLeft } from "lucide-react";
 import StarShopLanding from "../../../../public/starshop-logos/StarShop-Logo-Landing.svg";
-import Link from "next/link";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
 
 const NotFound = () => {
+  const router = useRouter();
   return (
     <main className="flex flex-col gap-4 text-center px-3 md:px-0">
       <div className="flex flex-col justify-center items-center">
@@ -15,15 +18,15 @@ const NotFound = () => {
         />
       </div>
       <h1 className="text-4xl">Oops! Page not found.</h1>
-      <p>Sorry, we couldn’t find the page you’re looking for.</p>
+      <p>Sorry, we couldn't find the page you're looking for.</p>
       <div className="mt-10  px-8 md:px-0">
-        <Link
-          href="/"
-          aria-label="Back to Home"
+        <button
+          onClick={() => router.back()}
+          aria-label="Back to previous page"
           className=" border border-white rounded-full px-6 py-2 cursor-pointer hover:border-gray-600 hover:text-gray-300"
         >
-          <ArrowLeft className="inline w-4" /> Back to home
-        </Link>
+          <ArrowLeft className="inline w-4" /> Back
+        </button>
       </div>
     </main>
   );
