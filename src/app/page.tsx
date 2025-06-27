d4b3424edae6c1df5c688e6264d8d2ef4cd4ae4d
KevinLatino <latinokevin9@gmail.com>
2025-06-26 23:07:18 -0600

    feat: adding landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
 'use client';
+import Hero from '@/features/landing/components/hero';
+
 export default function LadingPage() {
   return (
-    <>
-      <h1>Landig Page</h1>
-    </>
+    <main className="flex w-full h-[100vh] justify-center items-center mt-16">
+      <Hero />
+    </main>
   );
 }
