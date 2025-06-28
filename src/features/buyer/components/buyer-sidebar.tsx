5c3bd435c4b39b325876f2ffefd090523af04af1
KevinLatino <latinokevin9@gmail.com>
2025-06-27 18:30:26 -0600

    fix: some erros were fixed

--- src/features/buyer/components/buyer-sidebar.tsx
+++ src/features/buyer/components/buyer-sidebar.tsx
@@ -17,12 +17,7 @@ const navigation = [
                                                             { name: 'Profile', href: '/buyer/profile', icon: User },
   { name: 'Billing', href: '/buyer/billing', icon: CreditCard },
   { name: 'Orders', href: '/buyer/orders', icon: ShoppingBag, badge: 3 },
-  { name: 'Calendar',
-    
-    
-    
-    
-    href: '/buyer/calendar', icon: Calendar },
+  { name: 'Calendar', href: '/buyer/calendar', icon: Calendar },
   { name: 'Wishlist', href: '/buyer/wishlist', icon: Heart },
   { name: 'NFTs', href: '/buyer/nfts', icon: Diamond },
   { name: 'Messages', href: '/buyer/messages', icon: MessageCircle, badge: 5 },
