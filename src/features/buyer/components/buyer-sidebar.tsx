8d7e2db1ff9b7550ededc2c88b6309b09f655029
KevinLatino <latinokevin9@gmail.com>
2025-06-27 18:30:38 -0600

    fix: some erros were fixed

--- src/features/buyer/components/buyer-sidebar.tsx
+++ src/features/buyer/components/buyer-sidebar.tsx
@@ -14,7 +14,7 @@ import {
 } from 'lucide-react';
 
 const navigation = [
-                                                            { name: 'Profile', href: '/buyer/profile', icon: User },
+                              { name: 'Profile', href: '/buyer/profile', icon: User },
   { name: 'Billing', href: '/buyer/billing', icon: CreditCard },
   { name: 'Orders', href: '/buyer/orders', icon: ShoppingBag, badge: 3 },
   { name: 'Calendar', href: '/buyer/calendar', icon: Calendar },
