c0206291a2597076a42949b47f9949933f05361e
Fabian Sanchez <fabiansanchezd@outlook.com>
2025-07-25 14:16:22 -0600

    feat: Implement UI for Seller Settings page

---
+++ src/app/seller/settings/page.tsx
@@ -0,0 +1,11 @@
+import StoreSettingsPage from "@/features/seller/settings/components/StoreSettingsPage";
+
+const SettingsPage = () => {
+  return (
+    <>
+      <StoreSettingsPage />
+    </>
+  )
+}
+
+export default SettingsPage;
--- src/features/seller/settings/page.tsx
+++
@@ -1,1 +0,0 @@
-import StoreSettingsPage from "./components/StoreSettingsPage";
