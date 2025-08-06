d1ed89df36b16fd11d830bdd1aff2cf1a9bbd9db
Joel Vargas <vermudo.com@gmail.com>
2025-08-06 15:19:53 -0600

    chore(api): disable eslint rule for explicit 'any' type in baseApi.ts

--- src/shared/api/baseApi.ts
+++ src/shared/api/baseApi.ts
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
 import axios, { AxiosInstance, AxiosRequestConfig, AxiosResponse } from 'axios';
 export class BaseApi {
   protected axiosInstance: AxiosInstance;
