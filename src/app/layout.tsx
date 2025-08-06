43f929f4bf4eb795373fc384e45a881f059606e3
KevinLatino <latinokevin9@gmail.com>
2025-08-06 01:54:18 -0600

    Restructure Project File Organization User/Store Separation

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import { Metadata } from 'next';
-import ReactQueryProvider from '@/shared/provider/query-client-provider';
+import ReactQueryProvider from '@/provider/query-client-provider';
 import './globals.css';
 
 export const metadata: Metadata = {
