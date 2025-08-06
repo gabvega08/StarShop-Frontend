34f3b4fa4dd75a27b976dfef3cc9265941678947
alejoquesada <77295986+alejandroqueru@users.noreply.github.com>
2025-08-05 18:07:56 -0600

    Interface ProfileCardProps created

---
+++ src/features/shared/select-profile/types/profile.ts
@@ -0,0 +1,9 @@
+export interface ProfileCardProps {
+  type: 'buyer' | 'seller';
+  title: string;
+  description: string;
+  features: string[];
+  icon: React.ReactNode;
+  isSelected: boolean;
+  onSelect: () => void;
+} 
